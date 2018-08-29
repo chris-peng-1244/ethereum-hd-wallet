f854b8c3b75b00a12fc1552f535c4d2fa82c4638
ChrisPeng <pengchi1244@gmail.com>
2018-08-29 20:51:02 +0800

    Finish the test

--- test/controllers/account.js
+++ test/controllers/account.js
@@ -4,13 +4,21 @@ chai.should();
 chai.use(chaiHttp);
 
 const app = require('../../lib/index').default;
+const {Account, sequelize} = require('../../lib/models');
 
 describe("/account", () => {
+    beforeEach(async() => {
+        await sequelize.query('TRUNCATE TABLE accounts');
+    });
+
     it("It should create a new account", async() => {
         const res = await chai.request(app)
             .post('/accounts')
             .type('application/json')
             .send({ userId: 1 });
-        console.log(res.body);
+        res.body.address.length.should.equal(42);
+        res.body.address.should.match(/^0x.{40}/);
+        const account = await Account.findOne({where:{address: res.body.address}});
+        account.userId.should.equal(1);
     });
 });
